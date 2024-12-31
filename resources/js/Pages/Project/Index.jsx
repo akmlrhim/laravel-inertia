6bc334773c9f5600dba7c988818e81ba27d2e12e
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2024-12-31 08:56:40 +0800

    add component

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,10 +1,26 @@
 import Pagination from "@/Components/Pagination";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
+import {
+  PROJECT_STATUS_CLASS_MAP,
+  PROJECT_STATUS_TEXT_MAP,
+} from "@/constants.jsx";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, Link } from "@inertiajs/react";
 
-export default function Index({ projects }) {
+export default function Index({ auth, projects, queryParams = null }) {
+  queryParams = queryParams || {};
+  const searchFieldChanged = (name, value) => {
+    if (value) {
+      queryParams[name] = value;
+    } else {
+      delete queryParams[name];
+    }
+  };
+
   return (
     <AuthenticatedLayout
+      user={auth.user}
       header={
         <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
           Projects
@@ -20,7 +36,7 @@ export default function Index({ projects }) {
               <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
                   <tr className="text-nowrap">
-                    <td className="px-3 py-3">ID</td>
+                    <td className="px-3 py-3">No</td>
                     <td className="px-3 py-3">Image</td>
                     <td className="px-3 py-3">Name</td>
                     <td className="px-3 py-3">Status</td>
@@ -28,6 +44,34 @@ export default function Index({ projects }) {
                     <td className="px-3 py-3">Due Date</td>
                     <td className="px-3 py-3">Created By</td>
                     <td className="px-3 py-3 text-right">Actions</td>
+                  </tr>
+                </thead>
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                  <tr className="text-nowrap">
+                    <td className="px-3 py-3"></td>
+                    <td className="px-3 py-3"></td>
+                    <td className="px-3 py-3">
+                      <TextInput
+                        className="w-full"
+                        placeholder="Project Name"
+                        onBlur={(e) =>
+                          searchFieldChanged("name", e.target.value)
+                        }
+                        onKeyPress={(e) => onKeyPress("name", e.target.value)}
+                      />
+                    </td>
+                    <td className="px-3 py-3">
+                      <SelectInput
+                        className="w-full"
+                        onChange={(e) =>
+                          searchFieldChanged("status", e.target.value)
+                        }
+                      />
+                    </td>
+                    <td className="px-3 py-3"></td>
+                    <td className="px-3 py-3"></td>
+                    <td className="px-3 py-3"></td>
+                    <td className="px-3 py-3 text-right"></td>
                   </tr>
                 </thead>
                 <tbody>
@@ -45,7 +89,16 @@ export default function Index({ projects }) {
                         />
                       </td>
                       <td className="px-3 py-2">{project.name}</td>
-                      <td className="px-3 py-2">{project.status}</td>
+                      <td className="px-3 py-2">
+                        <span
+                          className={
+                            "px-2 py-1 rounded text-white " +
+                            PROJECT_STATUS_CLASS_MAP[project.status]
+                          }
+                        >
+                          {PROJECT_STATUS_TEXT_MAP[project.status]}
+                        </span>
+                      </td>
                       <td className="px-3 py-2 text-nowrap">
                         {project.created_at}
                       </td>
